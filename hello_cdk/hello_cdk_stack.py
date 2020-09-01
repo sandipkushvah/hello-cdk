d8d343f17fa176f524341edcb234f65910f4464f
sandipkushvah <sandipkushvah@gmail.com>
2020-09-02 00:23:05 +0530

    second commit

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,5 +1,7 @@
 from aws_cdk import (
     aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
     core
 )
 
@@ -10,6 +12,18 @@ class HelloCdkStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
-        bucket = s3.Bucket(self,
-    "MyFirstCDKBucket",
-    versioned=True,)
+        # bucket = s3.Bucket(self, "MyFirstCDKBucket", versioned=True,) # Used to create bucket
+
+         # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
+
+
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda,
+        )
